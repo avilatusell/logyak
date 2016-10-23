cc44613e02f195283fdbd5a9d9442adf22af8dae
avilatusell <avilatusell@gmail.com>
2016-10-23 16:57:46 +0200

    Some more changes for 3scale plugin via code

--- index.js
+++ index.js
@@ -4,26 +4,10 @@ var db = require('./server/db');
 var dbURI= process.env.DB_URI;
 var PORT = process.env.PORT || 8080;
 
+
 db.open(dbURI);
 
 app.listen( PORT, function() {
 	console.log('Now running on port 8080');
 })
-
-
-//API authentication configured in 3scale to use the user_key mode
-var ThreeScale = require('3scale').Client;
-// keep your provider key secret
-var client = new ThreeScale("c754f1b79120bd23a36d9fb9b124859b");
-
-// you will usually obtain user_key from request params
-client.authrep_with_user_key({ user_key: "099970efb91529e64a35eddfcdbb4ff6" }, function(response){
-  if(response.is_success()) {
-    // continue
-  } else {
-    throw new Error("not authorized " + response.error_message);
-  }
-});
-
-
 
--- server/app.js
+++ server/app.js
@@ -2,11 +2,38 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes');
 var compression = require('compression');
 
+
+var routes = require('./routes');
+
+
 var app = express();
+
+
+
+
+var THREESCALE_PROVIDER_KEY=process.env.THREESCALE_PROVIDER_KEY;
+
+//API authentication configured in 3scale to use the user_key mode
+var ThreeScale = require('3scale').Client;
+// keep your provider key secret
+var client = new ThreeScale(THREESCALE_PROVIDER_KEY);
+
+app.all('/api/*', function(request, response, next){
+	// you will usually obtain user_key from request params
+	client.authrep_with_user_key(
+		{ user_key: "099970efb91529e64a35eddfcdbb4ff6" , "usage": { "hits": 1 } }, 
+	function(response3S){
+	  if(response3S.is_success()) {
+	    next();
+	  } else {
+	    res.status(403).send( "not authorized " + response3S.error_message);
+	  }
+	});
+});
+
+
 
 // for HTML rendering
 app.use(express.static(path.join( __dirname, '../client')));
